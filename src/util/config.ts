0e33fad7eb69a999929e1c6c3e01954c09f81751
chenli13 <chenli13@asiainfo.com>
2023-05-30 10:39:00 +0800

    fix pass

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,14 +1,15 @@
 export const baseConf = {
-  "relay": {
-      "server": "la.alichen.top:3443",
-      "uuid": "abc",
-      "password": "foo",
-      "congestion_control": "bbr"
+  relay: {
+    server: "la.alichen.top:3443",
+    uuid: "abc",
+    password: "foo",
+    congestion_control: "bbr",
+    alpn: ["h3", "spdy/3.1"],
   },
-  "local": {
-    "server": "[::]:1086",
-    "dual_stack": true,
-    "max_packet_size": 1500
+  local: {
+    server: "[::]:1086",
+    dual_stack: true,
+    max_packet_size: 1500,
   },
-  "log_level": "info"
-}
+  log_level: "info",
+};
